e7379543f92ac76cd7dfe70e11611fb7ca9af6fc
jdv52 <jaysondv0928@gmail.com>
2024-07-09 14:32:08 -0400

    fixed all include errors hopefully

--- include/dac/ml_dac0.h
+++ include/dac/ml_dac0.h
@@ -7,11 +7,10 @@
 #define ML_DAC0_H
 
 #include <Arduino.h>
-#include <ml_dac_common.h>
+#include <dac/ml_dac_common.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 void DAC0_init(void);
@@ -37,38 +36,38 @@ void DAC0_write(const uint16_t value);
 void DAC0_buffer_write(const uint16_t value);
 uint16_t DAC0_get_interpol_result(void);
 
-#define ML_DAC0_GET_READY_STATUS()          (DAC->STATUS.bit.READY0)
-#define ML_DAC0_GET_EOC_STATUS()            (DAC->STATUS.bit.EOC0)
+#define ML_DAC0_GET_READY_STATUS() (DAC->STATUS.bit.READY0)
+#define ML_DAC0_GET_EOC_STATUS() (DAC->STATUS.bit.EOC0)
 
 // EVCTRL is PAC write protected
-#define ML_DAC0_STARTEI_ENABLE()            (DAC->EVCTRL.bit.STARTEI0 = 0x01)
-#define ML_DAC0_STARTEI_DISABLE()           (DAC->EVCTRL.bit.STARTEI0 = 0x00)
-#define ML_DAC0_EMPTYEO_ENABLE()            (DAC->EVCTRL.bit.EMPTYEO0 = 0x01)
-#define ML_DAC0_EMPTYEO_DISABLE()           (DAC->EVCTRL.bit.EMPTYEO0 = 0x00)
-#define ML_DAC0_INVEI_ENABLE()              (DAC->EVCTRL.bit.INVEI0 = 0x01)
-#define ML_DAC0_INVEI_DISABLE()             (DAC->EVCTRL.bit.INVEI0 = 0x00)
-#define ML_DAC0_RESRDYEO_ENABLE()           (DAC->EVCTRL.bit.RESRDYEO0 = 0x01)
-#define ML_DAC0_RESRDYEO_DISABLE()          (DAC->EVCTRL.bit.RESRDYEO0 = 0x00)
+#define ML_DAC0_STARTEI_ENABLE() (DAC->EVCTRL.bit.STARTEI0 = 0x01)
+#define ML_DAC0_STARTEI_DISABLE() (DAC->EVCTRL.bit.STARTEI0 = 0x00)
+#define ML_DAC0_EMPTYEO_ENABLE() (DAC->EVCTRL.bit.EMPTYEO0 = 0x01)
+#define ML_DAC0_EMPTYEO_DISABLE() (DAC->EVCTRL.bit.EMPTYEO0 = 0x00)
+#define ML_DAC0_INVEI_ENABLE() (DAC->EVCTRL.bit.INVEI0 = 0x01)
+#define ML_DAC0_INVEI_DISABLE() (DAC->EVCTRL.bit.INVEI0 = 0x00)
+#define ML_DAC0_RESRDYEO_ENABLE() (DAC->EVCTRL.bit.RESRDYEO0 = 0x01)
+#define ML_DAC0_RESRDYEO_DISABLE() (DAC->EVCTRL.bit.RESRDYEO0 = 0x00)
 
-#define ML_DAC0_INTSET_UNDERRUN()           (DAC->INTENSET.bit.UNDERRUN0 = 0x01)
-#define ML_DAC0_INTSET_EMPTY()              (DAC->INTENSET.bit.EMPTY0 = 0x01)
-#define ML_DAC0_INTSET_RESRDY()             (DAC->INTENSET.bit.RESRDY0 = 0x01)
-#define ML_DAC0_INTSET_OVERRUN()            (DAC->INTENSET.bit.OVERRUN0 = 0x01)
+#define ML_DAC0_INTSET_UNDERRUN() (DAC->INTENSET.bit.UNDERRUN0 = 0x01)
+#define ML_DAC0_INTSET_EMPTY() (DAC->INTENSET.bit.EMPTY0 = 0x01)
+#define ML_DAC0_INTSET_RESRDY() (DAC->INTENSET.bit.RESRDY0 = 0x01)
+#define ML_DAC0_INTSET_OVERRUN() (DAC->INTENSET.bit.OVERRUN0 = 0x01)
 
-#define ML_DAC0_INTCLR_UNDERRUN()           (DAC->INTENCLR.bit.UNDERRUN0 = 0x01)
-#define ML_DAC0_INTCLR_EMPTY()              (DAC->INTENCLR.bit.EMPTY0 = 0x01)
-#define ML_DAC0_INTCLR_RESRDY()             (DAC->INTENCLR.bit.RESRDY0 = 0x01)
-#define ML_DAC0_INTCLR_OVERRUN()            (DAC->INTENCLR.bit.OVERRUN0 = 0x01)
+#define ML_DAC0_INTCLR_UNDERRUN() (DAC->INTENCLR.bit.UNDERRUN0 = 0x01)
+#define ML_DAC0_INTCLR_EMPTY() (DAC->INTENCLR.bit.EMPTY0 = 0x01)
+#define ML_DAC0_INTCLR_RESRDY() (DAC->INTENCLR.bit.RESRDY0 = 0x01)
+#define ML_DAC0_INTCLR_OVERRUN() (DAC->INTENCLR.bit.OVERRUN0 = 0x01)
 
-#define ML_DAC0_UNDERRUN_CLR_INTFLAG()      (DAC->INTFLAG.bit.UNDERRUN0 = 0x01)
-#define ML_DAC0_EMPTY_CLR_INTFLAG()         (DAC->INTFLAG.bit.EMPTY0 = 0x01)
-#define ML_DAC0_RESRDY_CLR_INTFLAG()        (DAC->INTFLAG.bit.RESRDY0 = 0x01)
-#define ML_DAC0_OVERRUN_CLR_INTFLAG()       (DAC->INTFLAG.bit.OVERRUN0 = 0x01)
+#define ML_DAC0_UNDERRUN_CLR_INTFLAG() (DAC->INTFLAG.bit.UNDERRUN0 = 0x01)
+#define ML_DAC0_EMPTY_CLR_INTFLAG() (DAC->INTFLAG.bit.EMPTY0 = 0x01)
+#define ML_DAC0_RESRDY_CLR_INTFLAG() (DAC->INTFLAG.bit.RESRDY0 = 0x01)
+#define ML_DAC0_OVERRUN_CLR_INTFLAG() (DAC->INTFLAG.bit.OVERRUN0 = 0x01)
 
-#define ML_DAC0_UNDERRUN_INTFLAG()          (DAC->INTFLAG.bit.UNDERRUN0 == 0x01)
-#define ML_DAC0_EMPTY_INTFLAG()             (DAC->INTFLAG.bit.EMPTY0 == 0x01)
-#define ML_DAC0_RESRDY_INTFLAG()            (DAC->INTFLAG.bit.RESRDY0 == 0x01)
-#define ML_DAC0_OVERRUN_INTFLAG()           (DAC->INTFLAG.bit.OVERRUN0 == 0x01)
+#define ML_DAC0_UNDERRUN_INTFLAG() (DAC->INTFLAG.bit.UNDERRUN0 == 0x01)
+#define ML_DAC0_EMPTY_INTFLAG() (DAC->INTFLAG.bit.EMPTY0 == 0x01)
+#define ML_DAC0_RESRDY_INTFLAG() (DAC->INTFLAG.bit.RESRDY0 == 0x01)
+#define ML_DAC0_OVERRUN_INTFLAG() (DAC->INTFLAG.bit.OVERRUN0 == 0x01)
 
 #ifdef __cplusplus
 }
